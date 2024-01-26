8606df544207eed90320d174160cca53ec7f0c8a
roshantajne <rtajne7012@gmail.com>
2024-01-26 17:22:34 +0530

    adding files

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+    strings: ["Web Developer", "solution Provider", "Tech Enthusiast"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+    });
